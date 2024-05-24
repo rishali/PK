a835cd149e483178a92b43c2966f6172b30028ed
RISHALI <rrishali@gmail.com>
2024-05-25 00:47:43 +0100

    created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GuessComponent } from './components/guess/guess.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResultComponent } from './components/result/result.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    GuessComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ResultComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/guess/guess.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guess',
+  templateUrl: './guess.component.html',
+  styleUrl: './guess.component.scss'
+})
+export class GuessComponent {
+
+}
